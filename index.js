cd9c1c667d58ad3a74ee6190abde69808e2da365
UzairLeite <102726763+UzairLeite@users.noreply.github.com>
2024-04-25 13:48:33 +0200

    First Push

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://shopping-cart-app-87e4c-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
